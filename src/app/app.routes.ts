2250ee8c62e748543401163745ae603064392103
Islam-abdelwahed <islamabdelwahed61@gmail.com>
2026-03-13 23:55:20 +0200

    lab-02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,4 @@ import { Routes } from '@angular/router';
 import { Layout } from './layout/layout';
 import { Student } from './student/student';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: Layout,
-    children: [
-      { path: '', redirectTo: 'students', pathMatch: 'full' },
-      { path: 'students', component: Student },
-    ],
-  },
-];
+export const routes: Routes = [];
--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 interface StudentItem {
   id: number;
@@ -8,18 +9,56 @@ interface StudentItem {
   grade: string;
 }
 
+
 @Component({
   selector: 'app-student',
-  imports: [CommonModule],
+  imports: [FormsModule],
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
 export class Student {
   students: StudentItem[] = [
     { id: 1, name: 'Alice Johnson', age: 20, grade: 'A' },
-    { id: 2, name: 'Bob Smith',    age: 22, grade: 'B' },
-    { id: 3, name: 'Carol White',  age: 21, grade: 'A' },
-    { id: 4, name: 'David Brown',  age: 23, grade: 'C' },
+    { id: 2, name: 'Bob Smith', age: 22, grade: 'B' },
+    { id: 3, name: 'Carol White', age: 21, grade: 'A' },
+    { id: 4, name: 'David Brown', age: 23, grade: 'C' },
     { id: 5, name: 'Eva Martinez', age: 20, grade: 'B' },
   ];
+
+  isAddView = false;
+  newStudent = {
+    name: '',
+    age: 0,
+    grade: '',
+  };
+
+  showListView(): void {
+    this.isAddView = false;
+  }
+
+  showAddView(): void {
+    this.isAddView = true;
+  }
+
+  addStudent(): void {
+    if (!this.newStudent.name.trim() || !this.newStudent.grade.trim()) {
+      return;
+    }
+
+    const nextId = this.students.length + 1;
+
+    this.students.push({
+      id: nextId,
+      name: this.newStudent.name.trim(),
+      age: this.newStudent.age,
+      grade: this.newStudent.grade.trim().toUpperCase(),
+    });
+
+    this.newStudent = {
+      name: '',
+      age: 18,
+      grade: '',
+    };
+    this.showListView();
+  }
 }
